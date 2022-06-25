68595355662e168b0f07f49e4a64d19869adbd11
Rodrigo Alfaro Pinto <ralf@netstream.cl>
2022-06-25 17:52:41 -0400

    add custom.js

---
+++ assets/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    var endpoint = {
+      url_prod:"https://protemax.cl/cotizador/index.php/",
+      url_dev:"http://localhost:8888/php/cotizador/index.php/"
+    }
+});
